07456d00d4bc16e34f890afe32de073936c03fba
amywychan <amywingyichan@gmail.com>
2018-12-15 11:24:55 +0800

    initial

---
+++ src/app/destinations.service.ts
@@ -0,0 +1,37 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+
+@Injectable({
+  providedIn: 'root'
+})
+export class DestinationsService {
+
+  uri='https://morning-temple-35412.herokuapp.com/';
+
+  constructor(private http: HttpClient) { }
+
+  getDestinations() {
+    return this
+          .http
+          .get(`${this.uri}/destinations`);
+  }
+
+  getDestinationsById(id){
+    return this
+          .http
+          .get(`${this.uri}/destinations/${id}`);
+  } 
+
+  likeDestinations(id){
+    return this
+          .http
+          .get(`${this.uri}/destinations/like/${id}`);
+  } 
+
+  getDestinationsByPref(pref){
+    return this
+          .http
+          .get(`${this.uri}/destinations/${pref}`);
+  } 
+}
